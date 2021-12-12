b435891a62cb97dab24ccd6287e4930b0d8e7fdc
mdshohidulislam1486 <Shohidul Islam>
2021-12-13 00:56:28 +0600

    updated

--- src/page/Home/Travel/Travel.js
+++ src/page/Home/Travel/Travel.js
@@ -3,6 +3,7 @@ import {Box, Container, Typography, Grid} from '@mui/material';
 import travel1 from '../../../images/Travel/travel1.png'
 import travel2 from '../../../images/Travel/travel2.png'
 import travel3 from '../../../images/Travel/travel5.png'
+import './Travel.css'
 
 const Travel = () => {
     return (
@@ -17,13 +18,14 @@ const Travel = () => {
           </Box>
           <Box sx={{ width: '100%' }}>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid xs={12}  item sm={6} md={4}>
+                    <Grid className='travel-img' xs={12}  item sm={6} md={4}>
+                        
                         <img style={{position:'relative', width:'100%'}} src={travel1} alt="" />
                     </Grid>
-                    <Grid xs={12}  item sm={6} md={4}>
+                    <Grid className='travel-img' xs={12}  item sm={6} md={4}>
                      <img style={{position:'relative', width:'100%'}} src={travel2} alt="" />
                     </Grid>
-                    <Grid  xs={12}  item sm={6} md={4}>
+                    <Grid className='travel-img'  xs={12}  item sm={6} md={4}>
                     <img style={{position:'relative', width:'100%'}} src={travel3} alt="" />
                  
                     </Grid>
