bc9cdc666fdbff77c3e11384d845cf510211253a
mdshohidulislam1486 <Shohidul Islam>
2021-12-21 00:58:55 +0600

    updated

--- src/page/MyPorjects/MyProjects.js
+++ src/page/MyPorjects/MyProjects.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
 import { Box, Container } from '@mui/material';
 import MyProject from './MyProject';
+import Navigation from '../shared/Navigation/Navigation';
 
 const MyProjects = () => {
 const [projects, setProjects]= useState([])
@@ -15,6 +16,8 @@ useEffect(() =>{
 
 
     return (
+        <>
+        <Navigation></Navigation>
         <Container>
             <Box sx={{mt:8, display:'flex', flexWrap:'wrap', justifyContent:'center'}}>
                 
@@ -28,6 +31,7 @@ useEffect(() =>{
 
             </Box>
         </Container>
+        </>
     );
 };
 
--- src/page/shared/Navigation/Navigation.js
+++ src/page/shared/Navigation/Navigation.js
@@ -44,7 +44,7 @@ const Navigation = () => {
                  <h2 className='home-bac' >Developer</h2>
                  <ul>
                      <li  onClick={handleCloseMenu}><Link  to='/home'>Home</Link></li>
-                     <li><Link to="/home">Experience</Link></li>
+                     <li><Link onClick={handleCloseMenu} to="/myprojects">My-Projects</Link></li>
                      <li><a  target='_blank' rel="noreferrer" href="https://drive.google.com/file/d/1xyV3Mec1zRegJTJaZv0GWp-3ML5ncky2/view?usp=sharing" alt='ffh'>Resume</a></li>
                      <li><Link onClick={handleCloseMenu} to="/contact">Contact</Link></li>
                      <li><Link to="/home">About</Link></li>
