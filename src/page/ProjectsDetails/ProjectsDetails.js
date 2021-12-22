b710a2c23f3319859ebe92732b2dfe9d68bc2167
mdshohidulislam1486 <Shohidul Islam>
2021-12-23 01:45:53 +0600

    updated

--- src/page/ProjectsDetails/ProjectsDetails.js
+++ src/page/ProjectsDetails/ProjectsDetails.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import { Container, Box, Grid, Typography, ListItemText } from '@mui/material';
+import Navigation from '../shared/Navigation/Navigation';
 
 
 const ProjectsDetails = () => {
@@ -24,10 +26,84 @@ const ProjectsDetails = () => {
 
 
     return (
-        <div>
-            <h2>{currentProject?.name} {projectid}</h2>
 
-        </div>
+        <Container>
+            <Navigation/>
+            <Box>
+                <Grid container spacing={2} sx={{mt:7, display:'flex', alignItems:'center'}}>
+                    <Grid item xs={12} md={6} sx={{justifyContent:'center', display:'flex'}}>
+                        <Box sx={{width:'90%', height:'90%', position:'relative'}}>
+                        <img style={{position:'relative', width:'100%'}} src={currentProject?.img1} alt="" />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={6} sx={{justifyContent:'center', display:'flex'}}>
+                        <Box sx={{width:'90%', height:'90%', position:'relative'}}>
+                            <Typography variant='h5'>
+                                Used Teachnology
+                            </Typography>
+                            <Typography variant='body1'>
+                                {currentProject?.usedTeachnology}
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    </Grid>
+                    <Grid container spacing={2} sx={{mt:2, display:'flex', alignItems:'center'}}>
+                    
+                    <Grid item xs={12} md={6} sx={{justifyContent:'center', display:'flex'}}>
+                        <Box sx={{width:'90%', height:'90%', position:'relative'}}>
+                            <Typography variant='h5'>
+                               Main Features
+                            </Typography>
+                            <Box>
+                            <ListItemText
+                                primary="For Users"
+                                secondary={
+                                    <React.Fragment>
+                                    <Typography
+                                        sx={{ display: 'inline' }}
+                                        component="span"
+                                        variant="body2"
+                                        color="text.primary"
+                                    >
+                                        {currentProject?.feature1}
+                                    </Typography>
+                                    <br/>
+                                    <Typography
+                                        sx={{ display: 'inline' }}
+                                        component="span"
+                                        variant="body2"
+                                        color="text.primary"
+                                    >
+                                        {currentProject?.feature2}
+                                    </Typography>
+                                    <Typography
+                                        sx={{ display: 'inline' }}
+                                        component="span"
+                                        variant="body2"
+                                        color="text.primary"
+                                    >
+                                        {currentProject?.feature3}
+                                    </Typography>
+                            
+                                    </React.Fragment>
+                                }
+                                />
+
+                            </Box>
+                            
+                        </Box>
+                    </Grid>
+                    
+                    <Grid item xs={12} md={6} sx={{justifyContent:'center', display:'flex'}}>
+                       
+                    <Box  sx={{width:'90%', height:'90%', position:'relative'}}>
+                         <img style={{position:'relative', width:'100%'}} src={currentProject?.img2} alt="" />
+                    </Box>
+                
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
     );
 };
 
