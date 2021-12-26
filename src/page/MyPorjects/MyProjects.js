843ad32c0a4f6478b9c8b815d4b4464b837dbb66
mdshohidulislam1486 <Shohidul Islam>
2021-12-26 23:53:59 +0600

    updated

--- src/page/MyPorjects/MyProjects.js
+++ src/page/MyPorjects/MyProjects.js
@@ -3,8 +3,8 @@ import Typography from '@mui/material/Typography';
 import { Box, Container } from '@mui/material';
 import MyProject from './MyProject';
 import Navigation from '../shared/Navigation/Navigation';
-import Stack from '@mui/material/Stack';
-import LinearProgress from '@mui/material/LinearProgress';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 const MyProjects = () => {
 const [projects, setProjects]= useState([])
@@ -22,20 +22,15 @@ useEffect(() =>{
         <Navigation></Navigation>
         <Container>
             <Box sx={{mt:8, display:'flex', flexWrap:'wrap', justifyContent:'center'}}>
-
                 { projects
                     ? projects.map(project =><MyProject
                     key={project._id}
                     project={project}
                     ></MyProject>)
                     :
-                    <Box sx={{justifyContent:'center', alignItems:'center', display:'flex'}}>
-                        <Stack sx={{ width: '70%', color: 'grey.500' }} spacing={2}>
-                            <LinearProgress color="inherit" />
-                            <LinearProgress color="success" />
-                            <LinearProgress color="primary" />
-                        </Stack>
-                    </Box>
+                    <Box sx={{ display: 'flex' }}>
+                    <CircularProgress />
+                  </Box>
                 }
                 
 
